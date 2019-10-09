e4f8cbd8add8e46f79f1f2773b6fd0794ddbc23d
ubuntu <ubuntu>
2019-10-09 02:19:58 +0000

    +

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char** argv) {
   try {
     LightGBM::Application app(argc, argv);
     app.Run();
-    profiler::dumpBlocksToFile("lightgbm.network.profile");
+    profiler::dumpBlocksToFile("lightgbm.network.prof");
   }
   catch (const std::exception& ex) {
     std::cerr << "Met Exceptions:" << std::endl;
