1441288fefc480effcd1291d3eb6a8feb1f7efb5
ubuntu <ubuntu>
2019-10-19 00:14:59 +0000

    synchronize LightGBM across PHub cluster

--- src/treelearner/data_parallel_tree_learner.cpp
+++ src/treelearner/data_parallel_tree_learner.cpp
@@ -153,7 +153,6 @@ void DataParallelTreeLearner<TREELEARNER_T>::InitializePHub()
     //sets phubcoreoffset to continue right after max core.
     setenv("PHubCoreOffset", getenv("PHubMaximumCore"), 1);
     setenv("PHubMaximumCore", "1", 1);
-    //fprintf(stderr, "PHubCoreOffset is set to %s\n", getenv("PHubMaximumCore"));
   }
   pHubAllReduceT3 = createPHubInstance(pHubBackingBufferForAllReduceT3.data(), 1, num_machines_, rank_, 1, PHubDataType::CUSTOM, PHUB_ALL_REDUCE_T3_KEY0_SIZE);
   pHubAllReduceT3->SetReductionFunction(&PHubTuple3Reducer);
@@ -162,8 +161,8 @@ void DataParallelTreeLearner<TREELEARNER_T>::InitializePHub()
   {
     //sets phubcoreoffset to continue right after max core.
     auto reqCore = reduceScatterCores + 1; //this is for reduce scatter
+    reqCore += 2;                                   //this is for T3 allreduce
     setenv("PHubCoreOffset", std::to_string(reqCore).c_str(), 1);
-    //fprintf(stderr, "PHubCoreOffset is set to %s\n", std::to_string(reqCore).c_str());    
   }
   int PHUB_ALL_REDUCE_SPLITINFO_KEY0_SIZE = 2 * SplitInfo::Size(this->config_->max_cat_threshold);
   pHubBackingBufferForAllReduceSplitInfo.resize(PHUB_ALL_REDUCE_SPLITINFO_KEY0_SIZE);
