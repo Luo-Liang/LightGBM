47971cf11ff8bda23a3de747cfbef0c86d91287d
ubuntu <ubuntu>
2019-10-30 22:17:33 +0000

    synchronize LightGBM across PHub cluster

--- src/io/dataset_loader.cpp
+++ src/io/dataset_loader.cpp
@@ -606,7 +606,7 @@ Dataset* DatasetLoader::CostructFromSampleData(double** sample_values,
     int num_machines = Network::num_machines();
     int rank = Network::rank();
     int total_num_feature = num_col;
-    fprintf(stderr, "GlobalSyncUpByMax. rank = %d, num_machines = %d\n", rank, num_machines);    
+    //fprintf(stderr, "GlobalSyncUpByMax. rank = %d, num_machines = %d\n", rank, num_machines);    
     total_num_feature = Network::GlobalSyncUpByMin(total_num_feature);
     // start and len will store the process feature indices for different machines
     // machine i will find bins for features in [ start[i], start[i] + len[i] )
@@ -652,7 +652,7 @@ Dataset* DatasetLoader::CostructFromSampleData(double** sample_values,
         max_bin = std::max(max_bin, bin_mappers[i]->num_bin());
       }
     }
-    fprintf(stderr, "GlobalSyncUpByMax. rank = %d, num_machines = %d\n", rank, num_machines);    
+    //fprintf(stderr, "GlobalSyncUpByMax. rank = %d, num_machines = %d\n", rank, num_machines);    
     max_bin = Network::GlobalSyncUpByMax(max_bin);
     // get size of bin mapper with max_bin size
     int type_size = BinMapper::SizeForSpecificBin(max_bin);
@@ -980,7 +980,7 @@ void DatasetLoader::ConstructBinMappersFromTextData(int rank, int num_machines, 
         max_bin = std::max(max_bin, bin_mappers[i]->num_bin());
       }
     }
-    fprintf(stderr, "GlobalSyncUpByMax. rank = %d, num_machines = %d\n", rank, num_machines);
+    //fprintf(stderr, "GlobalSyncUpByMax. rank = %d, num_machines = %d\n", rank, num_machines);
     max_bin = Network::GlobalSyncUpByMax(max_bin);
 
     // get size of bin mapper with max_bin size
