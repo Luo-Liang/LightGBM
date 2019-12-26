5e6a1f19b7909370d8c7848e2a7b57606089ff1e
ubuntu <ubuntu>
2019-12-26 03:36:56 +0000

    synchronize LightGBM across PHub cluster

--- src/main.cpp
+++ src/main.cpp
@@ -18,45 +18,24 @@ int main(int argc, char **argv)
   {
     EASY_PROFILER_DISABLE;
   }
-  try
+  auto launchWait = std::getenv("LAUNCH_WAIT");
+  if(launchWait != NULL)
   {
-    auto launchWait = std::getenv("LAUNCH_WAIT");
-    if(launchWait != NULL)
-    {
-      ///activate launch wait 
-      std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-    }
-    EASY_PROFILER_ENABLE;
-    LightGBM::Application app(argc, argv);
-    app.Run();
-    auto envFileName = std::getenv("LIGHTGBM_PROFILE_NAME");
-    std::string fName(envFileName == NULL ? "" : envFileName);
-    if (fName == "")
-    {
-      char filename[2048];
-      tm *timenow;
-      time_t now = time(NULL);
-      timenow = gmtime(&now);
-      strftime(filename, sizeof(filename), "lightgbm_%Y-%m-%d_%H-%M-%S.prof", timenow);
-      fName = std::string(filename);
-    }
-    profiler::dumpBlocksToFile(fName.c_str());
+    ///activate launch wait 
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
   }
-  catch (const std::exception &ex)
+  EASY_PROFILER_ENABLE;
+  LightGBM::Application app(argc, argv);
+  app.Run();
+  auto envFileName = std::getenv("LIGHTGBM_PROFILE_NAME");
+  std::string fName(envFileName == NULL ? "" : envFileName);
+  if (fName == "")
   {
-    std::cerr << "Met Exceptions:" << std::endl;
-    std::cerr << ex.what() << std::endl;
-    exit(-1);
-  }
-  catch (const std::string &ex)
-  {
-    std::cerr << "Met Exceptions:" << std::endl;
-    std::cerr << ex << std::endl;
-    exit(-1);
-  }
-  catch (...)
-  {
-    std::cerr << "Unknown Exceptions" << std::endl;
-    exit(-1);
+    char filename[2048];
+    tm *timenow;
+    time_t now = time(NULL);
+    timenow = gmtime(&now);
+    strftime(filename, sizeof(filename), "lightgbm_%Y-%m-%d_%H-%M-%S.prof", timenow);
+    fName = std::string(filename);
   }
 }
